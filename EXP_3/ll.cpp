f87f475a60bd97784e09067d71ddd91ad5a14db7
DeveloperDowny <vedantpanchal12345@gmail.com>
2022-10-11 19:46:22 +0530

    after refactoring exp3

--- EXP_3/ll.cpp
+++ EXP_3/ll.cpp
@@ -25,17 +25,15 @@ public:
     Node *head;
     LL()
     {
-        // head = new Node(1, 1);
 
-        // head->next = nullptr;
         head = nullptr;
     }
     ~LL() {}
 
-    void printLL()
+    void printLL(string message)
     {
         Node *temp = head;
-        cout << "Printing LL" << endl;
+        cout << message << ": ";
         while (temp != nullptr)
         {
             cout << temp->data << "x^" << temp->expo << " + ";
@@ -57,50 +55,9 @@ public:
         temp->next = nullptr;
     }
 
-    void multiply(LL eq2)
-    {
-        Node *temp = head;
-
-        while (temp != nullptr)
-        {
-            Node *temp2 = eq2.head;
-            while (temp2 != nullptr)
-            {
-                int res = temp->data * temp2->data;
-                int expo = temp->expo + temp2->expo;
-                Node *toMakeChangesTo = temp;
-                while (toMakeChangesTo != nullptr)
-                {
-                    if (toMakeChangesTo->expo == expo)
-                    {
-                        if (toMakeChangesTo == temp)
-                        {
-                            break;
-                        }
-                        toMakeChangesTo->data += res;
-                        break;
-                    }
-                    else
-                    {
-                        toMakeChangesTo = toMakeChangesTo->next;
-                    }
-                }
-
-                // temp->data *= temp2->data;
-                // temp->expo += temp->expo;
-                temp2 = temp2->next;
-            }
-
-            temp = temp->next;
-        }
-    }
-
     void multiply(LL eq1, LL eq2)
     {
-
         Node *temp = eq1.head;
-        // head = temp;
-        // Node *temp3 = head;
 
         while (temp != nullptr)
         {
@@ -130,45 +87,16 @@ public:
                         temp3 = temp3->next;
                     }
 
-                    // if (temp3->next == nullptr){
-                    //     temp3->next = new Node
-                    // }
-
-                    // if (temp3->next->expo == resExpo)
                     if (temp3->next == nullptr)
                     {
                         temp3->next = new Node(prodCoef, resExpo);
                         temp3->next->next = nullptr;
                     }
-                    else // (temp3->next->expo == resExpo)
+                    else
                     {
                         temp3->next->data += prodCoef;
                     }
-                    // else
-                    // {
-                    //     temp3->next = new Node(prodCoef, resExpo);
-                    // }
                 }
-
-                // if (temp3 == nullptr)
-                // {
-                //     head = new Node(prodCoef, resExpo);
-                //     head->next = nullptr;
-                //     temp3 = head;
-                // }
-                // else if (temp3->expo == resExpo)
-                // {
-                //     temp3->data += prodCoef;
-                // }
-                // else if (temp3->next != nullptr)
-                // {
-                //     temp3 = temp3->next;
-                //     temp3->data += prodCoef;
-                // }
-                // else
-                // {
-                //     temp3->next = new Node(prodCoef, resExpo);
-                // }
 
                 temp2 = temp2->next;
             }
@@ -324,29 +252,11 @@ int main(int argc, char const *argv[])
     eq2.addNodeAt(new Node(2, 1), -1);
     eq2.addNodeAt(new Node(4, 2), -1);
 
-    eq1.printLL();
-    eq2.printLL();
-
-    // eq1.multiply(eq2);
-    // eq1.printLL();
+    eq1.printLL("Equation 1 (e1)");
+    eq2.printLL("Equation 2 (e2)");
 
     result.multiply(eq1, eq2);
-    result.printLL();
-    // eq2.printLL();
-
-    // LL mLL = LL();
-
-    // mLL.testFunc();
-    // cout << "Initial LL" << endl;
-    // mLL.printLL();
-
-    // mLL.addNodeAt(new Node(0, 0), 1);
-    // cout << "After adding at 1st position" << endl;
-    // mLL.printLL();
-
-    // mLL.deleteNodeAt(1);
-    // cout << "After deleting at 1st position" << endl;
-    // mLL.printLL();
+    result.printLL("Product of equations (e1)*(e2)");
 
     return 0;
 }
